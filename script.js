72638f81b17f23d6d5c895afdd14454ec63e60f2
noodleums <149272985+noodleums@users.noreply.github.com>
2025-10-30 11:52:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const upg3 = document.getElementById("upgrade3");
 const upg4 = document.getElementById("upgrade4");
 const achievementsList = document.getElementById("achievements");
 const debug1 = document.getElementById("debug1");
+const rebirthCount = document.getElementById("rebirthCount");
+const rebirthB = document.getElementById("rebirth");
+const rebirthPro = document.getElementById("rebirthProgress");
 
 let upg1Cost = 25;
 let upg2Cost = 50;
@@ -23,19 +26,19 @@ const playerData = {
   upg3Lvl: 0,
   upg4Lvl: 0,
   achievementsArray: [],
-  achievement1: false,
-  achievement2: false,
-  achievement3: false,
-  achievement4: false
+  rebirths: 0
 }
 
-achievementsList.innerHTML = "Achievements:<br>";
+rebirth.style.visibility = "hidden";
 
 function initGame() {
-  clickCount.innerHTML = `clicks: ${playerData.clicks}`;
+  clickCount.innerHTML = `clicks: ${playerData.clicks.toFixed(2)}`;
+  rebirthCount.innerHTML = `rebirths: ${playerData.rebirths}`;
   loadData();
   cpsCalc();
   update();
+  achievements();
+  redrawAchievements();
 }
 function saveData() {
   localStorage.setItem("playerData", JSON.stringify(playerData));
@@ -44,15 +47,12 @@ function loadData() {
   const saveData = localStorage.getItem("playerData");
   if (saveData) {
     const saved = JSON.parse(saveData);
-    playerData.clicks = saved.clicks || 99;
-    playerData.upg1Lvl = saved.upg1Lvl || 0;
+    playerData.clicks = saved.clicks || 0;
+    playerData.upg1Lvl = saved.upg1Lvl || 1;
     playerData.upg2Lvl = saved.upg2Lvl || 0;
     playerData.upg3Lvl = saved.upg3Lvl || 0;
     playerData.upg4Lvl = saved.upg4Lvl || 0;
-    playerData.achievement1 = saved.achievement1 || false;
-    playerData.achievement2 = saved.achievement2 || false;
-    playerData.achievement3 = saved.achievement3 || false;
-    playerData.achievement4 = saved.achievement4 || false;
+    playerData.rebirths = saved.rebirths || 0;
     playerData.achievementsArray = saved.achievementsArray || [];
     upg1Cost = calcUpg1Cost(playerData.upg1Lvl);
     upg2Cost = calcUpg2Cost(playerData.upg2Lvl);
@@ -62,14 +62,54 @@ function loadData() {
 }
 function update() {
   upgCount = (playerData.upg1Lvl-1)+playerData.upg2Lvl+playerData.upg3Lvl+playerData.upg4Lvl;
-  clickCount.innerHTML = `clicks: ${playerData.clicks}`;
-  cpsCount.innerHTML = `clicks per second: ${playerData.upg2Lvl+(playerData.upg3Lvl*10)+(playerData.upg4Lvl*25)}`;
+  clickCount.innerHTML = `clicks: ${playerData.clicks.toFixed(2)}`;
+  rebirthCount.innerHTML = `rebirths: ${playerData.rebirths}`;
+  cpsCount.innerHTML = `clicks per second: ${playerData.upg2Lvl+(playerData.upg3Lvl*10)+(playerData.upg4Lvl*50)}`;
   upg1.innerHTML = `${playerData.upg1Lvl} clicks per click<br>cost to upgrade: ${upg1Cost}`;
   upg2.innerHTML = `${playerData.upg2Lvl} clicks/s<br>cost to upgrade: ${upg2Cost}`;
   upg3.innerHTML = `${playerData.upg3Lvl*10} clicks/s<br>cost to upgrade: ${upg3Cost}`;
-  upg4.innerHTML = `${playerData.upg4Lvl*25} clicks/s<br>cost to upgrade: ${upg4Cost}`;
+  upg4.innerHTML = `${playerData.upg4Lvl*50} clicks/s<br>cost to upgrade: ${upg4Cost}`;
+  rebirthF();
+  rebirthProgress();
   achievements();
   saveData();
+}
+function rebirthF() {
+  if (playerData.rebirths == 0 && playerData.clicks >= 100000) {
+    rebirthB.innerHTML = "Rebirth"
+    rebirthB.style.visibility = "visible";
+    rebirthB.addEventListener("click", function() {
+      rebirthing();
+      playerData.rebirths = 1;
+      rebirthB.style.visibility = "hidden";
+    });
+  }
+  if (playerData.rebirths == 1 && playerData.clicks >= 500000) {
+    rebirthB.innerHTML = "Rebirth"
+    rebirthB.style.visibility = "visible";
+    rebirthB.addEventListener("click", function() {
+      rebirthing();
+      playerData.rebirths = 2;
+      rebirthB.style.visibility = "hidden";
+    });
+  }
+}
+function rebirthProgress() {
+  if (playerData.rebirths == 0) {
+    rebirthPro.max = 100000;
+    rebirthPro.value = playerData.clicks;
+  }
+  if (playerData.rebirths == 1) {
+    rebirthPro.max = 500000;
+    rebirthPro.value = playerData.clicks;
+  }
+}
+function rebirthing() {
+  playerData.clicks = 30032;
+  playerData.upg1Lvl = 1;
+  playerData.upg2Lvl = 0;
+  playerData.upg3Lvl = 0;
+  playerData.upg4Lvl = 0;
 }
 function achievements() {
   if (playerData.clicks >= 100 && !playerData.achievementsArray.includes("Get 100 clicks<br>")) {
@@ -86,6 +126,14 @@ function achievements() {
   }
   if (playerData.clicks >= 10000 && !playerData.achievementsArray.includes("Get 10,000 clicks<br>")) {
     playerData.achievementsArray.push("Get 10,000 clicks<br>");
+    redrawAchievements();
+  }
+  if (playerData.clicks >= 100000 && !playerData.achievementsArray.includes("Get 100,000 clicks<br>")) {
+    playerData.achievementsArray.push("Get 100,000 clicks<br>");
+    redrawAchievements();
+  }
+  if (playerData.rebirths >= 1 && !playerData.achievementsArray.includes("Rebirth 1 time<br>")) {
+    playerData.achievementsArray.push("Rebirth 1 time<br>");
     redrawAchievements();
   }
 }
@@ -134,7 +182,6 @@ function upg2Button() {
     update();
   }
 }
-
 function upg3Button() {
   if (playerData.clicks >= upg3Cost){
     playerData.clicks -= upg3Cost;
@@ -155,7 +202,8 @@ function upg4Button() {
 }
 function cpsCalc() {
   setInterval(function() {
-    playerData.clicks += playerData.upg2Lvl+(playerData.upg3Lvl*10)+(playerData.upg4Lvl*25);
+    let cps = (playerData.upg2Lvl+(playerData.upg3Lvl*10)+(playerData.upg4Lvl*50));
+    playerData.clicks += cps + (cps*(playerData.rebirths/10));
     saveData();
     update();
   }, 1000);
@@ -163,7 +211,8 @@ function cpsCalc() {
 function clearSaveData() {
   const clearData = prompt("Are you sure?\nIf so type \"yes\"");
   if (clearData.toLowerCase().trim() == "yes") {
-    playerData.clicks = 99;
+    playerData.clicks = 0;
+    playerData.rebirths = 0;
     playerData.upg1Lvl = 1;
     playerData.upg2Lvl = 0;
     playerData.upg3Lvl = 0;
@@ -172,13 +221,11 @@ function clearSaveData() {
     upg2Cost = 50;
     upg3Cost = 100;
     upg4Cost = 500;
-    playerData.achievement1 = false;
-    playerData.achievement2 = false;
-    playerData.achievement3 = false;
-    playerData.achievement4 = false;
     playerData.achievementsArray = [];
+    localStorage.clear();
     saveData();
     location.reload();
   }
 }
+
 document.addEventListener('DOMContentLoaded', initGame);
